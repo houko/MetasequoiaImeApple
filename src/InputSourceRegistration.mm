c1e1611b08302a606ba6f766409a882d429db698
Evan <suzukaze.haduki@gmail.com>
2026-09-03 21:01:20 -0700

    fix(install): enable registered input source (#124)

--- src/InputSourceRegistration.mm
+++ src/InputSourceRegistration.mm
@@ -16,3 +16,78 @@ OSStatus MetasequoiaRegisterInputSource(NSURL *bundleURL, MetasequoiaInputSource
     }
     return registrar((__bridge CFURLRef)bundleURL);
 }
+
+OSStatus MetasequoiaRegisterAndEnableInputSources(NSURL *bundleURL, NSString *bundleIdentifier,
+                                                  MetasequoiaInputSourceRegistrar registrar,
+                                                  MetasequoiaInputSourceLister lister,
+                                                  MetasequoiaInputSourcePropertyGetter propertyGetter,
+                                                  MetasequoiaInputSourceEnabler enabler)
+{
+    if (bundleIdentifier.length == 0 || lister == nullptr || propertyGetter == nullptr || enabler == nullptr)
+    {
+        return paramErr;
+    }
+    OSStatus status = MetasequoiaRegisterInputSource(bundleURL, registrar);
+    if (status != noErr)
+    {
+        return status;
+    }
+
+    NSDictionary *filter = @{
+        (__bridge NSString *)kTISPropertyBundleID : bundleIdentifier,
+        (__bridge NSString *)kTISPropertyInputSourceIsEnableCapable : @YES,
+    };
+    CFArrayRef sources = lister((__bridge CFDictionaryRef)filter, true);
+    if (sources == nullptr || CFArrayGetCount(sources) == 0)
+    {
+        if (sources != nullptr)
+        {
+            CFRelease(sources);
+        }
+        return fnfErr;
+    }
+
+    bool enabledParent = false;
+    CFIndex sourceCount = CFArrayGetCount(sources);
+    for (CFIndex index = 0; index < sourceCount; ++index)
+    {
+        TISInputSourceRef source = (TISInputSourceRef)CFArrayGetValueAtIndex(sources, index);
+        void *property = propertyGetter(source, kTISPropertyInputSourceID);
+        if (property == nullptr || CFGetTypeID(property) != CFStringGetTypeID() ||
+            ![(__bridge NSString *)property isEqualToString:bundleIdentifier])
+        {
+            continue;
+        }
+        status = enabler(source);
+        if (status != noErr)
+        {
+            CFRelease(sources);
+            return status;
+        }
+        enabledParent = true;
+    }
+    if (!enabledParent)
+    {
+        CFRelease(sources);
+        return fnfErr;
+    }
+
+    for (CFIndex index = 0; index < sourceCount; ++index)
+    {
+        TISInputSourceRef source = (TISInputSourceRef)CFArrayGetValueAtIndex(sources, index);
+        void *property = propertyGetter(source, kTISPropertyInputSourceID);
+        if (property != nullptr && CFGetTypeID(property) == CFStringGetTypeID() &&
+            [(__bridge NSString *)property isEqualToString:bundleIdentifier])
+        {
+            continue;
+        }
+        status = enabler(source);
+        if (status != noErr)
+        {
+            CFRelease(sources);
+            return status;
+        }
+    }
+    CFRelease(sources);
+    return noErr;
+}
